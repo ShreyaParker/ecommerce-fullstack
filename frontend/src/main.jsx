159d45feeac4d20af6da2daa052d68400043620d
ShreyaParkar317 <parkshreya317@gmail.com>
2025-12-11 22:26:16 +0530

    fix: Hardcode Axios Base URL to Render Backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,9 @@ import App from './App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
+import axios from "axios";
+
+axios.defaults.baseURL = "https://ecommerce-fullstack-97s3.onrender.com";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
